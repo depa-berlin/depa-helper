fc0c2c3bb2599de7a2446473544a268dc8eb8067
Mirko Fenrich <fenrich@designpark.de>
2021-12-08 00:07:12 +0100

    shortenText

---
+++ src/Traits/shortenText.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Depa\Helper\Traits;
+
+trait shortenText
+{
+    /**
+     * @param string|null $string
+     * @param int $text
+     * @return string
+     */
+    private function shortenText(?string $text, int $length): string
+    {
+        $string = strip_tags($text);
+        if (strlen($string) > $length) {
+            $stringCut = substr($string, 0, $length);
+            $endPoint = strrpos($stringCut, ' ');
+            $string = $endPoint ? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+        }
+        return $string;
+    }
+}
